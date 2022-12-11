41b658e3547491a1c28b39d655a2583225d9dc7c
Kazuma Hashiguchi <brands.sari@icloud.com>
2022-12-11 05:31:00 +0000

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  
+  # ユーザー登録、ログイン認証が使われる前に、configure_permitted_parametersメソッドを実行する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # サインイン後の遷移先をアバウトページへ
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  # サインアウト後の遷移先をトップページへ
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
+  protected
+  
+  # ユーザー登録の際に、ユーザー名のデータ操作の許可をしている
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  
+    
+  devise_for :admins, skip: [:registrations, :passwords],controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  
+  
+  
+  
+  
+  
+  get '/homes/about' => 'homes#about', as: 'about'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
