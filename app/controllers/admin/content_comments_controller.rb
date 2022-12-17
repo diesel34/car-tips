47ad8f0c193a5c3c2529adb91726b0822c5c97b2
Kazuma Hashiguchi <brands.sari@icloud.com>
2022-12-17 10:53:57 +0000

    first

--- app/controllers/admin/content_comments_controller.rb
+++ app/controllers/admin/content_comments_controller.rb
@@ -3,6 +3,6 @@ class Admin::ContentCommentsController < ApplicationController
   # コメントの削除機能
   def destroy
     ContentComment.find(params[:id]).destroy
-    redirect_to admin_content_comment_path(params[:content_review_id])
+    redirect_to request.referer
   end
 end
